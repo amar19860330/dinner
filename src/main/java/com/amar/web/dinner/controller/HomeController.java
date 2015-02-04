6fdbaf7ee4a17820a13a14ba01fd7d4913151c39
yangheng33 <yangheng33@sina.com>
2015-02-05 03:37:23 +0800

    初始导入，可以使用springmvc的基本环境

---
+++ src/main/java/com/amar/web/dinner/controller/HomeController.java
@@ -0,0 +1,47 @@
+package com.amar.web.dinner.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping( "/home" )
+public class HomeController
+{
+	public final Logger log = Logger.getLogger( this.getClass() );
+
+	@RequestMapping( value = "/login/{userid}" , method = RequestMethod.GET )
+	public ModelAndView login( @PathVariable( "userid" ) Long userid )
+	{
+		log.info( "login:" + userid );
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.setViewName( "1" );
+		modelAndView.getModelMap().put( "username" , "" + userid );
+		return modelAndView;
+	}
+
+	@RequestMapping( value = "/logout/{userid}" , method = RequestMethod.GET )
+	public ModelAndView logout( @PathVariable( "userid" ) Long userid )
+	{
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.setViewName( "1" );
+		modelAndView.getModelMap().put( "username" , "" + userid );
+		return modelAndView;
+	}
+
+	@RequestMapping( value = "/blog/{blogId}/message/{msgId}" , method = RequestMethod.DELETE )
+	public ModelAndView delete( @PathVariable( "blogId" ) Long blogId , @PathVariable( "msgId" ) Long msgId , HttpServletRequest request , HttpServletResponse response )
+	{
+
+		return null;
+	}
+
+}
---
+++ src/main/java/com/amar/web/dinner/util/SpringBeanFactory.java
@@ -0,0 +1,19 @@
+package com.amar.web.dinner.util;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class SpringBeanFactory implements ApplicationContextAware
+{
+	private static ApplicationContext applicationContext;
+
+	public void setApplicationContext( ApplicationContext a )
+	{
+		applicationContext = a;
+	}
+
+	@SuppressWarnings( "unchecked" )
+	public static < T > T getBean( String beanName )
+	{
+		return ( T ) applicationContext.getBean( beanName );
+	}
+}
